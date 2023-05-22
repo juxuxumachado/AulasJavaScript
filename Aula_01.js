5b58c70575cc05d66198a7e01fb6c0de3648a205
Juliana Machado <78989166+juxuxumachado@users.noreply.github.com>
2023-05-22 20:32:16 -0300

    Alteracao do enunciado

--- Aula_01.js
+++ Aula_01.js
@@ -187,7 +187,8 @@ if( valor1 > valor2){
     }
 
     /*9 Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço 
-normal de etiqueta e a escolha da condição de pagamento. Utilize os códigos da tabela a seguir 
+normal de etiqueta e a escolha
+ da condição de pagamento. Utilize os códigos da tabela a seguir 
 para ler qual a condição de pagamento escolhida e efetuar o cálculo adequado.  */
 
 
